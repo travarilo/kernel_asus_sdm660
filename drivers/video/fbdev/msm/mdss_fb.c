77c2e4ae22e4ec1de334b25157d77a5f5ec56e9c
Albert I <krascgq@outlook.co.id>
2018-12-15 21:33:07 +0800

    X00T: msm: mdss: Sync changes with decrypted S337 kernel source
    Slightly modified:
    * Ignored log-related priority changes
    * Removed "[Display]" tag
    * printk -> pr_debug (same as any other framebuffer debugs)
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -82,6 +82,12 @@
  */
 #define MDP_TIME_PERIOD_CALC_FPS_US	1000000
 
+#ifdef CONFIG_MACH_ASUS_X00T
+static void asus_lcd_early_unblank_func(struct work_struct *);
+static struct workqueue_struct *asus_lcd_early_unblank_wq;
+extern int g_resume_from_fp;
+#endif
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 
@@ -103,6 +109,9 @@ static int mdss_fb_pan_display(struct fb_var_screeninfo *var,
 static int mdss_fb_check_var(struct fb_var_screeninfo *var,
 			     struct fb_info *info);
 static int mdss_fb_set_par(struct fb_info *info);
+#ifdef CONFIG_MACH_ASUS_X00T
+static int mdss_fb_blank(int blank_mode, struct fb_info *info);
+#endif
 static int mdss_fb_blank_sub(int blank_mode, struct fb_info *info,
 			     int op_enable);
 static int mdss_fb_suspend_sub(struct msm_fb_data_type *mfd);
@@ -1396,6 +1405,11 @@ static int mdss_fb_probe(struct platform_device *pdev)
 			pr_err("failed to register input handler\n");
 
 	INIT_DELAYED_WORK(&mfd->idle_notify_work, __mdss_fb_idle_notify_work);
+#ifdef CONFIG_MACH_ASUS_X00T
+	INIT_DELAYED_WORK(&mfd->early_unblank_work,
+				asus_lcd_early_unblank_func);
+	mfd->early_unblank_work_queued = false;
+#endif
 
 	return rc;
 }
@@ -1606,13 +1620,47 @@ static int mdss_fb_resume(struct platform_device *pdev)
 #endif
 
 #ifdef CONFIG_PM_SLEEP
+#ifdef CONFIG_MACH_ASUS_X00T
+static void asus_lcd_early_unblank_func(struct work_struct *work)
+{
+	struct delayed_work *dw = to_delayed_work(work);
+	struct msm_fb_data_type *mfd = container_of(dw, struct msm_fb_data_type,
+							early_unblank_work);
+	struct fb_info *fbi;
+
+	if (!mfd) {
+		pr_err("cannot get mfd from work\n");
+		return;
+	}
+
+	fbi = mfd->fbi;
+	if (!fbi)
+		return;
+
+	fb_blank(fbi, FB_BLANK_UNBLANK);
+
+	mfd->early_unblank_work_queued = false;
+}
+#endif
+
 static int mdss_fb_pm_suspend(struct device *dev)
 {
 	struct msm_fb_data_type *mfd = dev_get_drvdata(dev);
 	int rc = 0;
+#ifdef CONFIG_MACH_ASUS_X00T
+	struct fb_info *fbi;
+#endif
 
 	if (!mfd)
 		return -ENODEV;
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	fbi = mfd->fbi;
+	if (!fbi)
+		return -ENODEV;
+
+	fb_blank(fbi, FB_BLANK_POWERDOWN);
+#endif
 
 	dev_dbg(dev, "display pm suspend\n");
 
@@ -1638,6 +1686,9 @@ static int mdss_fb_pm_suspend(struct device *dev)
 static int mdss_fb_pm_resume(struct device *dev)
 {
 	struct msm_fb_data_type *mfd = dev_get_drvdata(dev);
+#ifdef CONFIG_MACH_ASUS_X00T
+	int rc = 0;
+#endif
 	if (!mfd)
 		return -ENODEV;
 
@@ -1655,7 +1706,22 @@ static int mdss_fb_pm_resume(struct device *dev)
 	if (mfd->mdp.footswitch_ctrl)
 		mfd->mdp.footswitch_ctrl(true);
 
+#ifdef CONFIG_MACH_ASUS_X00T
+	rc = mdss_fb_resume_sub(mfd);
+	if (g_resume_from_fp && mfd->index == 0) {
+		if (!mfd->early_unblank_work_queued) {
+			pr_debug("doing unblank from resume, due to fp.\n");
+			mfd->early_unblank_work_queued = true;
+			queue_delayed_work(asus_lcd_early_unblank_wq,
+						&mfd->early_unblank_work, 0);
+		} else
+			pr_debug("mfd->early_unblank_work_queued returns true.\n");
+	}
+
+	return rc;
+#else
 	return mdss_fb_resume_sub(mfd);
+#endif
 }
 #endif
 
@@ -5213,6 +5279,11 @@ int __init mdss_fb_init(void)
 
 	if (platform_driver_register(&mdss_fb_driver))
 		return rc;
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	asus_lcd_early_unblank_wq =
+			create_singlethread_workqueue("display_early_wq");
+#endif
 
 	return 0;
 }
