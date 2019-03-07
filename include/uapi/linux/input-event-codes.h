689f91c9d1f902ed95643e9278da135f2d6ecf3a
anomalchik <vidslava@yandex.ru>
2019-03-07 15:49:33 -0100

    X00T: drivers: input: touchscreen: Add touchscreens

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -666,6 +666,13 @@
 #define KEY_SARSENSOR_FAR		0x2eb
 #endif
 
+#define KEY_TP_GESTURE_W 	0x2ec
+#define KEY_TP_GESTURE_E 	0x2ed
+#define KEY_TP_GESTURE_S 	0x2ee
+#define KEY_TP_GESTURE_Z 	0x2ef
+#define KEY_TP_GESTURE_C 	0x2f0
+#define KEY_TP_GESTURE_V 	0x2f1
+
 /* We avoid low common keys in module aliases so they don't get huge. */
 #define KEY_MIN_INTERESTING	KEY_MUTE
 #define KEY_MAX			0x2ff
