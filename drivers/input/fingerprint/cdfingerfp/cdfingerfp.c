e14aa17eb7346e47c26c11455368c70bd63fdbf6
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-06-16 06:45:42 +0700

    input: cdfinger: mark IRQ as performance critical
    In order to improve fingerprint wake performance, mark the fingerprint
    reader's IRQ as performance-critical.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Change-Id: I16f48fa973cc0497986d974c84d02591d2ba6c12

--- drivers/input/fingerprint/cdfingerfp/cdfingerfp.c
+++ drivers/input/fingerprint/cdfingerfp/cdfingerfp.c
@@ -446,11 +446,11 @@ static int cdfinger_eint_gpio_init(struct cdfingerfp_data *pdata)
 */
 	/* Huaqin modify for cdfinger irq wake by leiyu at 2018/04/10 start */
 	int error = 0;
+	int irqf;
 	/* Huaqin modify for cpu_boost by leiyu at 2018/04/25 start */
 	//error = commonfp_request_irq(NULL,cdfinger_eint_handler, IRQF_TRIGGER_RISING|IRQF_ONESHOT,"cdfinger_eint", (void*)pdata);
-	error = commonfp_request_irq(cdfinger_eint_handler, NULL,
-				     IRQF_TRIGGER_RISING, "cdfinger_eint",
-				     (void *)pdata);
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL;
+	error = commonfp_request_irq(cdfinger_eint_handler,NULL, irqf, "cdfinger_eint", (void*)pdata);
 	/* Huaqin modify for cpu_boost by leiyu at 2018/04/25 end */
 	if (error < 0) {
 		CDFINGER_ERR("commonfp_request_irq error %d\n", error);
