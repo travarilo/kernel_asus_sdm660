5a62da6f115a7078bb54f1c468926aab3b222fa4
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-08-12 08:09:12 +0700

    sensors: import Sensortek STK3321 ALSPS sensor
    * based on S055 kernel source
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

---
+++ drivers/sensors/stk3x1x/stk3x1x.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2012~2015 Lex Hsieh <lex_hsieh@sensortek.com.tw>
+ */
+
+#ifndef __STK3X1X_H__
+#define __STK3X1X_H__
+
+/* platform data */
+struct stk3x1x_platform_data {
+	uint8_t state_reg;
+	uint8_t psctrl_reg;
+	uint8_t alsctrl_reg;
+	uint8_t ledctrl_reg;
+	uint8_t	wait_reg;
+	uint16_t ps_thd_h;
+	uint16_t ps_thd_l;
+	int int_pin;
+	uint32_t transmittance;
+	uint32_t int_flags;
+};
+
+#endif // __STK3X1X_H__
